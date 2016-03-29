11a9a4c372edd0f15f9e5a239e7974f0149aac0e
bradleyoashburn <bradleyoashburn@gmail.com>
2016-03-28 14:35:33 -1000

    All tests passing

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,8 +1,6 @@
-function ContactController($scope) {
-    $scope.name = 'Bill Gates';
-    $scope.email = 'william@microsoft.com'
+function ContactController() {
+  this.name = 'Bill Gates';
+  this.email = 'william@microsoft.com'
 }
 
-angular
-    .module('app')
-    .controller('ContactController', ContactController);
+angular.module('app').controller('ContactController', ContactController);
