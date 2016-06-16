1e331befb2f900179c0365794dbd198a5bf80d2e
seanliu93 <sean93liu@gmail.com>
2016-06-15 23:47:22 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,8 +1,7 @@
-function ContactController($scope) {
-    $scope.name = 'Bill Gates';
-    $scope.email = 'william@microsoft.com'
+function ContactController() {
+  this.name = 'Bill Gates';
+  this.email = 'william@microsoft.com'
 }
-
 angular
     .module('app')
     .controller('ContactController', ContactController);
